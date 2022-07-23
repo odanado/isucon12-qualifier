af0b994043ff1137df6ed9b5c8055b4be9787d05
mizdra <pp.mizdra@gmail.com>
2022-07-23 17:19:50 +0900

    init.sh おそすぎ問題修正

--- sql/init.sql
+++ sql/init.sql
@@ -8,4 +8,9 @@ DELETE FROM player WHERE created_at >= '1654041600';
 DELETE FROM player_score WHERE created_at >= '1654041600';
 
 -- 時間がかかるので別で実行済み
--- ALTER TABLE visit_history ADD INDEX tenant_competition_player_idx (tenant_id, competition_id, player_id);
+-- ALTER TABLE visit_history ADD INDEX tenant_competition_player_idx (created_at, competition_id, player_id);
+
+-- TODO(mizdra): isu2, isu3 にはまだ貼ってないので注意
+-- ALTER TABLE competition ADD INDEX created_at_idx (created_at);
+-- ALTER TABLE player ADD INDEX created_at_idx (created_at);
+-- ALTER TABLE player_score ADD INDEX created_at_idx (created_at);
