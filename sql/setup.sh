d0bfb6ec7aa6f03294d7400a6e1afd3895fd60f7
mizdra <pp.mizdra@gmail.com>
2022-07-23 16:30:07 +0900

    一瞬で insert されるようになった！！！

--- sql/setup.sh
+++ sql/setup.sh
@@ -8,9 +8,10 @@ cd `dirname $0`
 # テーブル作成
 mysql -uisucon -pisucon -Disuports < renew-table.sql
 
-for file in `ls ../../initial_data/1.db`; do
+for file in `ls ../../initial_data/*.db`; do
 	./sqlite3-to-csv $file
-	mysql -uisucon -pisucon -Disuports --enable-local-infile -e"load data local infile 'competition.csv' into table competition fields terminated by ',' OPTIONALLY ENCLOSED BY '\"';";
-	mysql -uisucon -pisucon -Disuports --enable-local-infile -e"load data local infile 'player.csv' into table player fields terminated by ',' OPTIONALLY ENCLOSED BY '\"';";
-	mysql -uisucon -pisucon -Disuports --enable-local-infile -e"load data local infile 'player_score.csv' into table player_score fields terminated by ',' OPTIONALLY ENCLOSED BY '\"';";
 done
+
+mysql -uisucon -pisucon -Disuports --enable-local-infile -e"load data local infile 'competition.csv' into table competition fields terminated by ',' OPTIONALLY ENCLOSED BY '\"';";
+mysql -uisucon -pisucon -Disuports --enable-local-infile -e"load data local infile 'player.csv' into table player fields terminated by ',' OPTIONALLY ENCLOSED BY '\"';";
+mysql -uisucon -pisucon -Disuports --enable-local-infile -e"load data local infile 'player_score.csv' into table player_score fields terminated by ',' OPTIONALLY ENCLOSED BY '\"';";
