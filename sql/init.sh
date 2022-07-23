c891c74a66a2bd09b7ac4f72e7b93442c13474a6
mizdra <pp.mizdra@gmail.com>
2022-07-23 15:00:00 +0900

    できた

--- sql/init.sh
+++ sql/init.sh
@@ -16,6 +16,18 @@ mysql -u"$ISUCON_DB_USER" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME" < init.sql
 
+rm competition.csv
+rm player.csv
+rm player_score.csv
+
+for file in `ls ../../initial_data/*.db`; do
+		./sqlite3-to-csv $file
+done
+
+mysql -uisucon -pisucon -Disuports --enable-local-infile -e"load data local infile 'competition.csv' into table competition fields terminated by ',';";
+mysql -uisucon -pisucon -Disuports --enable-local-infile -e"load data local infile 'player.csv' into table player fields terminated by ',';";
+mysql -uisucon -pisucon -Disuports --enable-local-infile -e"load data local infile 'player_score.csv' into table player_score fields terminated by ',';";
+
 # SQLiteのデータベースを初期化
 # rm -f ../tenant_db/*.db
 # cp -r ../../initial_data/*.db ../tenant_db/
--- sql/init.sql
+++ sql/init.sql
@@ -2,3 +2,38 @@ DELETE FROM tenant WHERE id > 100;
 DELETE FROM visit_history WHERE created_at >= '1654041600';
 UPDATE id_generator SET id=2678400000 WHERE stub='a';
 ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
+
+-- sqlite3 から移行してきたテーブルたち
+
+DROP TABLE IF EXISTS competition;
+DROP TABLE IF EXISTS player;
+DROP TABLE IF EXISTS player_score;
+
+CREATE TABLE competition (
+  id VARCHAR(255) NOT NULL PRIMARY KEY,
+  tenant_id BIGINT NOT NULL,
+  title TEXT NOT NULL,
+  finished_at BIGINT NULL,
+  created_at BIGINT NOT NULL,
+  updated_at BIGINT NOT NULL
+) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+
+CREATE TABLE player (
+  id VARCHAR(255) NOT NULL PRIMARY KEY,
+  tenant_id BIGINT NOT NULL,
+  display_name TEXT NOT NULL,
+  is_disqualified BOOLEAN NOT NULL,
+  created_at BIGINT NOT NULL,
+  updated_at BIGINT NOT NULL
+) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+
+CREATE TABLE player_score (
+  id VARCHAR(255) NOT NULL PRIMARY KEY,
+  tenant_id BIGINT NOT NULL,
+  player_id VARCHAR(255) NOT NULL,
+  competition_id VARCHAR(255) NOT NULL,
+  score BIGINT NOT NULL,
+  row_num BIGINT NOT NULL,
+  created_at BIGINT NOT NULL,
+  updated_at BIGINT NOT NULL
+) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
