2641f004f20a0c0364d1d83f596fa53f3cf3789a
odan <odan3240@gmail.com>
2022-07-23 11:04:28 +0900

    ディレクトリ名が違うよ

--- node/src/setup-tracing.ts
+++ node/src/setup-tracing.ts
@@ -2,7 +2,7 @@ import tracer, { Tracer } from "dd-trace";
 import { execSync } from "child_process"
 
 export function setupDatadog(): Tracer {
-  const currentCommitHash = execSync("cd ~/webapp/nodejs/; git show -s --format=%ci HEAD").toString().trim()
+  const currentCommitHash = execSync("cd ~/webapp/node/; git show -s --format=%ci HEAD").toString().trim()
   return tracer.init({
     version: currentCommitHash,
     profiling: true,
