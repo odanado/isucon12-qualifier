4dde8015dacdf3ffaac1b03720a25dafdfd004ec
mizdra <pp.mizdra@gmail.com>
2022-07-23 15:27:02 +0900

    ちょっとずつ csv を import してみる

--- sql/init.sh
+++ sql/init.sh
@@ -16,17 +16,12 @@ mysql -u"$ISUCON_DB_USER" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME" < init.sql
 
-rm competition.csv
-rm player.csv
-rm player_score.csv
-
 for file in `ls ../../initial_data/*.db`; do
-		./sqlite3-to-csv $file
+	./sqlite3-to-csv $file
+	mysql -uisucon -pisucon -Disuports --enable-local-infile -e"load data local infile 'competition.csv' into table competition fields terminated by ',';";
+	mysql -uisucon -pisucon -Disuports --enable-local-infile -e"load data local infile 'player.csv' into table player fields terminated by ',';";
+	mysql -uisucon -pisucon -Disuports --enable-local-infile -e"load data local infile 'player_score.csv' into table player_score fields terminated by ',';";
 done
-
-mysql -uisucon -pisucon -Disuports --enable-local-infile -e"load data local infile 'competition.csv' into table competition fields terminated by ',';";
-mysql -uisucon -pisucon -Disuports --enable-local-infile -e"load data local infile 'player.csv' into table player fields terminated by ',';";
-mysql -uisucon -pisucon -Disuports --enable-local-infile -e"load data local infile 'player_score.csv' into table player_score fields terminated by ',';";
 
 # SQLiteのデータベースを初期化
 # rm -f ../tenant_db/*.db
